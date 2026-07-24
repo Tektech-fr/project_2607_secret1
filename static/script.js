61b8a8216b4b34a50760ab50a87e4081ba5c7a12
Vandanjon <vandanjon@gmail.com>
2026-07-24 15:13:02 +0200

    [FEAT] : add event listener on grid's buttons

--- static/script.js
+++ static/script.js
@@ -1,1 +1,20 @@
-console.log("scriupt cahrgé");
+document
+  .querySelector("#main_board")
+  .addEventListener("pointerdown", onPointerDown);
+
+function onPointerDown(event) {
+  if (!(event.target instanceof Element)) {
+    return;
+  }
+
+  const button = event.target.closest("[data-cell]");
+
+  if (!(button instanceof HTMLButtonElement)) {
+    return;
+  }
+
+  const x = Number(button.dataset.x);
+  const y = Number(button.dataset.y);
+
+  console.log(x, y);
+}
