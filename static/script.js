e05b6465f427714f02e155b2209f7d50819b44de
Vandanjon <vandanjon@gmail.com>
2026-07-24 19:36:15 +0200

    [FEAT] : buttons become invisible on click

--- static/script.js
+++ static/script.js
@@ -1,20 +1,33 @@
-document
-  .querySelector("#main_board")
-  .addEventListener("pointerdown", onPointerDown);
+const board = document
+  .querySelector("#main_board");
+
+  board.addEventListener("pointerdown", onPointerDown);
 
 function onPointerDown(event) {
-  if (!(event.target instanceof Element)) {
+  const button = getCell(event);
+  
+  if (!button) {
     return;
   }
 
-  const button = event.target.closest("[data-cell]");
-
-  if (!(button instanceof HTMLButtonElement)) {
-    return;
-  }
+  button.dataset.visible = "false";
 
   const x = Number(button.dataset.x);
   const y = Number(button.dataset.y);
-
+  
   console.log(x, y);
+}
+
+function getCell(event) {
+  if (!(event.target instanceof Element)) {
+    return null;
+  }
+  
+  const button = event.target.closest("[data-cell]");
+  
+  if (!(button instanceof HTMLButtonElement)) {
+    return null;
+  }
+
+  return button;
 }
